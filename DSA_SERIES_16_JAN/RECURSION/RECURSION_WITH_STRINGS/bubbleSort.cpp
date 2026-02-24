79e59eb866c2b585f0790e9e9d5d04338c228e89
lakshya8839 <chalanalakshya5@gmail.com>
2026-02-24 18:55:30 +0530

    Recursion in Bubble Sorting Algorithm

---
+++ DSA_SERIES_16_JAN/RECURSION/RECURSION_WITH_STRINGS/bubbleSort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void sortArray(int *arr, int n) {
+
+    //base case
+    if(n == 0 || n == 1) {
+        return ;
+    }
+
+    //1 case solve karlia - larges element ko end me rakh dega
+    for(int i=0; i<n; i++) {
+        if(arr[i] > arr[i+1]){
+            swap(arr[i], arr[i+1]);
+        }
+    }
+
+    //Recursive Call
+    sortArray(arr, n-1);
+
+}
+
+int main() {
+
+    int arr[5] = {2,5,1,6,9};
+
+    sortArray(arr,5);
+
+    for(int i=0; i<5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
