33cbdf5735730aabaa95a928a1b81394ef2b9bbe
lakshya8839 <chalanalakshya5@gmail.com>
2026-03-04 18:06:50 +0530

    Made Objects fo Class - OOps Concepts

---
+++ DSA_SERIES_16_JAN/OOPS_CONCEPTS/intro.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+    //properities
+    int health;
+
+};
+
+int main() {
+
+    //creation of Object
+    Hero h1;
+
+    cout << "size : " << sizeof(h1) << endl;
+
+    return 0;
+}
