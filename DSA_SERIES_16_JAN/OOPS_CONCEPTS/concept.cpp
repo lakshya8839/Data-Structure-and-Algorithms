5fb40ceb2b45f4af0b3bed522cc6a53dfc9f3044
lakshya8839 <chalanalakshya5@gmail.com>
2026-03-12 18:52:07 +0530

    Applied Inhertence Concept - 4 Pillars of OOPs

---
+++ DSA_SERIES_16_JAN/OOPS_CONCEPTS/concept.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+
+    private:
+        string name;
+        int age;
+        int height;
+    public:
+    int getAge(){
+        return this->age;
+    }
+};
+
+
+
+int main(){
+
+    Student first;
+
+    cout << "sab sahi chalra hai" << endl; 
+
+
+
+    return 0;
+}
